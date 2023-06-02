59f5f19ed6ef568c2c14bfc3dae842c5eba48b87
Bkz-Amar <amarbouakaz91@gmail.com>
2023-06-02 22:49:53 +0100

    b_in_to_po.py (prettier code)

--- b_in_to_po.py
+++ b_in_to_po.py
@@ -1,11 +1,23 @@
-# is_operator :(0/not 0)
-def priority(x):
-    if x =='!':   return 5 # ¬ 
-    if x in ['⨁','⊕','^']:   return 4
-    if x in [".","*",'↑','/'] : return 3
-    if x in ["+","↓",'-']:  return 2
-    if x =='=':   return 1
-    if x in ["(",")"]:  return -1
+# operators :
+NegO   = ['!','¬']
+EqO    = ['=']
+DisO   = ["+"] # disjunction
+NDisO  = ["↓",'-'] # NOR
+
+ConO   = [".","*"]  # conjunction
+NConO  = ['↑','/'] # NAND
+XorO   = ['⨁','⊕','^']
+
+Bkts   = ["(",")"] 
+
+
+def priority(o): # o ==> operator
+    if o in NegO:   return 5 
+    if o in XorO:   return 4
+    if o in ConO or o in NConO : return 3
+    if o in DisO or o in NDisO:  return 2
+    if o in EqO :   return 1
+    if o in Bkts:  return -1
     return 0 
 
 def is_int(x):
@@ -13,27 +25,7 @@ def is_int(x):
     except TypeError:  return False
     return True
 
-def operation(x,y,o):
-    x,y = int(x),int(y)
-    
-    if o in ['+'] : return 1 if x or y else 0
-    if o in ["."]  : return 1 if x and y else 0
-    
-    if o in ["↓",'-'] : return 1 if not x and not y else 0
-    if o in ['↑','/'] : return 0 if x and y else 1
-    
-    if o in ['⨁','⊕','^'] : return 1 if x^y else 0
-    if o=='=' : return 1 if x==y else 0
-    return None
-
-def replace(exp,DItems):
-    for x in range(len(exp)):
-         if not priority(exp[x]):
-             exp[x] = DItems[exp[x]]
-    return exp
- 
-# def is_valid():pass          
-
+# this Function convert expresions from normal form "a+b*c" to the postfix form "b c * a +" 
 def postfix(op):
     stack = [None]
     new   = []
@@ -42,10 +34,9 @@ def postfix(op):
         if x ==' ':continue
         if not priority(x): 
             new.append(x)
-        else:
-            
-            if x=='!' or len(stack)==1 or stack[0]=='(' or x=='(' or priority(x)>priority(stack[0]) :
-                if stack[0]=='!' and x not in'!(':
+        else:  
+            if x in NegO or len(stack)==1 or stack[0]=='(' or x=='(' or priority(x)>priority(stack[0]) :
+                if stack[0] in NegO and x not in'!¬(':
                     new.append(stack[0])
                     stack.pop(0)
                     
@@ -64,14 +55,38 @@ def postfix(op):
         new.append(stack[0])
         stack.pop(0)
     return  new    
-   
+
+# the aim of this function is to replace the variables by (1/0) values every time from the Truth table.
+# it takes expresion (PFF) and dictionary of the variables and the values ex :  replace("a B +",{"a":1,"b":0} )
+def replace(exp,DItems):
+    for x in range(len(exp)):
+         if not priority(exp[x]):
+             exp[x] = DItems[exp[x]]
+    return exp
+ 
+# TODO : def is_valid():pass          
+
+def operation(x,y,o):
+    x,y = int(x),int(y)
+    
+    if o in DisO  : return 1 if x or y else 0
+    if o in ConO  : return 1 if x and y else 0
+    
+    if o in NDisO : return 1 if not x and not y else 0
+    if o in NConO : return 0 if x and y else 1
+    
+    if o in XorO  : return 1 if x^y else 0
+    if o in EqO   : return 1 if x==y else 0
+    return None
+
+# this function take the result of replace function and calculate it ex : BCalc("1 + 0") -> 1 ex2 : Bcalc( replace( postfix(exp) , DictOfVarsValues ) )
 def BCalc(op):
     stack=[None]
     for x in op:
         if is_int(x):
             stack.insert(0,x)
             
-        elif x== "!": #negation
+        elif x in NegO: #negation
             stack.insert(0,1 if int(stack[0]) == 0 else 0)
             stack.pop(1)
             
--- run.py
+++ run.py
@@ -4,8 +4,7 @@ use =1
 while use :
     os.system("cls")
     t1 = TT(input("write a boolean expresions : ")) 
-    t1.PrintInfo()
+    # t1.PrintInfo()
     t1.PrintT()
     use = 1 if input("do you want to reuse (y/n)? ").lower() in ["1",'y','yes'] else 0
                 
-  
