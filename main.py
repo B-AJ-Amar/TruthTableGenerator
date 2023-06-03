1f597dd7585eac628b6e5c49c1a9dabc8b196eef
Bkz-Amar <amarbouakaz91@gmail.com>
2023-06-03 15:46:11 +0100

    add some commants

--- main.py
+++ main.py
@@ -12,34 +12,57 @@ import os
 class TT:
     def __init__(self,Ex:str):
         
+        #This function takes a string as input For example: Ex = "a+b;b.c".
+        
+        # In this section, we split the expressions and store them in a list called self.Exs.
+        # The resulting list will be: self.Exs = ["a+b", "b.c"].
         self.Exs=Ex.split(';')
-        self.ELen = len(self.Exs)    
-        self.Items,self.ILen = [],0
+        self.ELen = len(self.Exs)  # number of exepresions  
+        
+        
+        # The expressions in self.Exs are converted from infix to postfix notation and stored in self.ExsP.
+        # For example:self.Exs = ["a+b", "b+c"] => self.ExsP = ["ab+", "bc+"]
         self.ExsP = [postfix(x) for x in self.Exs]
+        
+        
+        # Next, we extract the variables from the expressions and store them in a list called self.Items.
+        # For example, considering the previous example:self.ExsP = ["ab+", "bc+"] => self.Items = ["a", "b", "c"]
+        self.Items,self.ILen = [],0
         for y in self.ExsP:
             for x in y:
                 if x  not in self.Items and not  priority(x):
                     self.Items.append(x)
                     self.ILen += 1
-                    
-        self.Items.sort()
+        self.Items.sort() # sort the variables (optional)
+        
+        
+        
+        
+        # Creat The Table----------------------------------------------------------------------------
+        
+        
+        # The next step involves creating the table and computing the results.
+
+        # size : y = 2^variables + 1 
+        #        x = variables + results. in this case : vars = 3 (a,b,c) , results = 2 (a+b and b.c). total = 5
         self.TX,self.TY=self.ELen+self.ILen,pow(2,self.ILen)+1
-        self.Table = [[0 for _ in range( self.TX)] for _ in range(self.TY)] 
+        
+        self.Table = [[0 for _ in range( self.TX)] for _ in range(self.TY)] # creat empty table
         
         
-        # header =============================
+        # header [a , b , c , R1 , R2]
         for x in  range(self.ILen):
-            self.Table[0][x] = self.Items[x]
+            self.Table[0][x] = self.Items[x] 
             
         for x in  range(self.ILen,self.TX):
             self.Table[0][x] = f"R{x-self.ILen+1}"
 
             
-        # 0..00 ,0..01 ,0..11 ... =============
+        # 000 ,001 ,011 ... 
         for x in range(self.ILen):
             tmp,tcount = 0,pow(2,x)
             count = tcount
-            
+        
             for y in range(0,pow(2,self.ILen)) :
                 if not count : 
                     count = tcount
@@ -47,7 +70,10 @@ class TT:
                 self.Table[y+1][x] = tmp 
                 count -=1
         
-        self.calc()       
+        # calculate 
+        self.calc()
+        
+               
     def calc(self):
         for x in range(self.ELen):
             for y in range(1,self.TY):
